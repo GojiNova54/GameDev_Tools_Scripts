82f04cb2e29811f1573aa09e0c70ced9df5ad9e2
Sean Anderson <nova7prospekt@gmail.com>
2024-01-26 11:09:41 -0700

    Updated Vector3Data

--- SO/Vector3DataSO.cs
+++ SO/Vector3DataSO.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Vector3DataSO : ScriptableObject
 {
-    public Vector3 vectorValue;
+    public Vector3 Value;
+    
+    public void SetValue(Vector3 v3)
+    {
+        Value = v3;
+    }
+
+    public void SetValue(Transform t)
+    {
+        Value= t.position;
+    }
+    
+    public void SetTransform (Transform t)
+    {
+        t.position = Value;
+    }
 }
 
